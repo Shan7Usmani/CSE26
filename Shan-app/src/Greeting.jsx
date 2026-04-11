759cec3bb35cec3cd7a5ffb83c6ab832fe9c52f2
Shan Usmani <takhiro79@gmail.com>
2026-04-11 13:56:29 +0530

    fixed nested repos

---
+++ Shan-app/src/Greeting.jsx
@@ -0,0 +1,12 @@
+//type rfce and then hit enter
+import React from 'react'
+
+function Greeting() {
+  return (
+    <div>
+      Greeting HOla Como Estas
+    </div>
+  )
+}
+
+export default Greeting
